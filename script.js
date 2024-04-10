f91f490ca46cdaca332a7d8ab0002cd36d32abf6
Arjun <arjun280603@gmail.com>
2024-04-10 18:49:05 +0530

    logo modify

--- script.js
+++ script.js
@@ -5,3 +5,70 @@ menubar.onclick = () =>{
     menubar.classList.toggle('fa-times');
     navbar.classList.toggle('active')
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the eye icon element
+    var eyeIcon1 = document.getElementById("eye-icon1");
+     // Add click event listener
+     eyeIcon1.addEventListener("click", function() {
+        // Navigate to another HTML page
+        window.open("departments/dept1.html", "_blank");
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the eye icon element
+    var eyeIcon2 = document.getElementById("eye-icon2");
+     // Add click event listener
+     eyeIcon2.addEventListener("click", function() {
+        // Navigate to another HTML page
+        window.open("departments/dept2.html", "_blank");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the eye icon element
+    var eyeIcon3 = document.getElementById("eye-icon3");
+     // Add click event listener
+     eyeIcon3.addEventListener("click", function() {
+        // Navigate to another HTML page
+        window.open("departments/dept3.html", "_blank");
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the eye icon element
+    var eyeIcon4 = document.getElementById("eye-icon4");
+     // Add click event listener
+     eyeIcon4.addEventListener("click", function() {
+        // Navigate to another HTML page
+        window.open("departments/dept4.html", "_blank");
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the eye icon element
+    var eyeIcon5 = document.getElementById("eye-icon5");
+     // Add click event listener
+     eyeIcon5.addEventListener("click", function() {
+        // Navigate to another HTML page
+        window.open("departments/dept5.html", "_blank");
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the eye icon element
+    var eyeIcon6 = document.getElementById("eye-icon6");
+     // Add click event listener
+     eyeIcon6.addEventListener("click", function() {
+        // Navigate to another HTML page
+        window.open("departments/dept6.html", "_blank");
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
